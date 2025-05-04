5bfc3d1135920a0089a75dd52cd197ef97e1c3e0
manojd929 <manoj.d.929@gmail.com>
2025-05-04 18:10:20 +0530

    feat: group anagram

--- neetcode-150/group-anagrams.test.js
+++ neetcode-150/group-anagrams.test.js
@@ -29,14 +29,6 @@ strs[i] is made up of lowercase English letters.
 
 */
 
-const validAnagram = (str1, str2) => {
-    if (str1.length !== str2.length) {
-        return false
-    }
-
-    return str1.split('').sort().join('') === str2.split('').sort().join('')
-}
-
 const groupAnagrams = (list = []) => {
     const result = {}
 
