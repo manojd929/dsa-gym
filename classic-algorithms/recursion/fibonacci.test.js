99e7f2776e566e62a5dbc0d171f4cbc85210fd1a
manojd929 <manoj.d.929@gmail.com>
2025-05-05 10:11:45 +0530

    feat: print fibonacci

--- classic-algorithms/recursion/fibonacci.test.js
+++ classic-algorithms/recursion/fibonacci.test.js
@@ -1,17 +1,23 @@
 const getFibonacci = (n) => {
-    if (n <= 1) {
-        return n;
-    }
+    if (n === 0) return 0;
+    if (n === 1) return 1;
 
     return getFibonacci(n - 1) + getFibonacci(n - 2);
 }
 
+const printFibonacci = (n) => {
+    let result = []
+    for (let i = 0; i < n; i++) {
+        result.push(getFibonacci(i))
+    }
+    return result.join('->')
+}
+
 describe('Fibonacci', () => {
     it('Test', () => {
-        expect(getFibonacci(0)).toBe(0)
-        expect(getFibonacci(1)).toBe(1)
-        expect(getFibonacci(2)).toBe(1)
-        expect(getFibonacci(3)).toBe(2)
-        expect(getFibonacci(4)).toBe(3)
+        expect(printFibonacci(1)).toBe('0')
+        expect(printFibonacci(2)).toBe('0->1')
+        expect(printFibonacci(3)).toBe('0->1->1')
+        expect(printFibonacci(10)).toBe('0->1->1->2->3->5->8->13->21->34')
     })
 })
