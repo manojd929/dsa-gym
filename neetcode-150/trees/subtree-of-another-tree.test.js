ce8748cf8975d7b8f37a7247a5e86f180d66fc1a
manojd929 <manoj.d.929@gmail.com>
2025-05-05 09:23:02 +0530

    feat: .

--- neetcode-150/trees/subtree-of-another-tree.test.js
+++ neetcode-150/trees/subtree-of-another-tree.test.js
@@ -1,3 +1,27 @@
+/*
+Subtree of Another Tree
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+Example 1:
+
+Input: root = [1,2,3,4,5], subRoot = [2,4,5]
+
+Output: true
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,null,6], subRoot = [2,4,5]
+
+Output: false
+
+Constraints:
+
+0 <= The number of nodes in both trees <= 100.
+-100 <= root.val, subRoot.val <= 100
+*/
+
 class TreeNode {
     constructor(data = 0) {
         this.data = data;
@@ -16,7 +40,7 @@ tree.right.right = new TreeNode(8)
 tree.right.right.left = new TreeNode(7)
 tree.right.right.right = new TreeNode(9)
 
-const tree1 =  new TreeNode(2)
+const tree1 = new TreeNode(2)
 tree1.left = new TreeNode(1)
 tree1.left = new TreeNode(3)
 
