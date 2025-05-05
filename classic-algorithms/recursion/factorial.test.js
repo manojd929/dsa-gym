df119c37a70c063a824c3b14d20492a2bb77269a
manojd929 <manoj.d.929@gmail.com>
2025-05-05 10:15:19 +0530

    feat: factorial of a number

--- classic-algorithms/recursion/factorial.test.js
+++ classic-algorithms/recursion/factorial.test.js
@@ -1,14 +1,32 @@
-const factorial = (num) => {
-  if (num <= 1) {
-    return 1;
+class Factorial {
+  static recursive(num) {
+    if (num <= 0) {
+      return 1;
+    }
+    return num * Factorial.recursive(num - 1);
   }
-  return num * factorial(num - 1);
-};
+
+  static iterative(num) {
+    if (num <= 1) {
+      return 1
+    }
+
+    let result = 1;
+
+    for (let i = 2; i <= num; i++) {
+      result *= i
+    }
+    return result
+  }
+}
 
 describe('Factorial N', () => {
   test('Test Case', () => {
-    expect(factorial(0)).toBe(1);
-    expect(factorial(5)).toBe(120);
-    expect(factorial(10)).toBe(3628800);
+    expect(Factorial.recursive(0)).toBe(1);
+    expect(Factorial.recursive(5)).toBe(120);
+    expect(Factorial.recursive(10)).toBe(3628800);
+    expect(Factorial.iterative(0)).toBe(1);
+    expect(Factorial.iterative(5)).toBe(120);
+    expect(Factorial.iterative(10)).toBe(3628800);
   });
 });
